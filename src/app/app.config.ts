7ff793715b1412ddda676ff13bf4188afc72ddda
Alex Saez <144711047+SaezAlex@users.noreply.github.com>
2026-09-25 16:00:14 +0200

    Update app.config.ts

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -15,6 +15,6 @@ export const appConfig: ApplicationConfig = {
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     {provide: LOCALE_ID, useValue: 'es' },
-    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "torneo-24h-castro", appId: "1:3411430392:web:11974cc65e444f906c240d", databaseURL: "https://torneo-24h-castro-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "torneo-24h-castro.firebasestorage.app", apiKey: "AIzaSyD3_WuZmBKjxP43A7Wny-M4Hiptq_t108s", authDomain: "torneo-24h-castro.firebaseapp.com", messagingSenderId: "3411430392"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
+    provideRouter(routes), provideFirebaseApp(() => initializeApp({ projectId: "torneo-24h-castro", appId: "1:3411430392:web:11974cc65e444f906c240d", databaseURL: "https://torneo-24h-castro-default-rtdb.europe-west1.firebasedatabase.app", storageBucket: "torneo-24h-castro.firebasestorage.app", apiKey: "", authDomain: "torneo-24h-castro.firebaseapp.com", messagingSenderId: "3411430392"})), provideAuth(() => getAuth()), provideFirestore(() => getFirestore())
   ]
 };
